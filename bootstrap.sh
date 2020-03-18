f8831bd1a71005f5dbbabdbbe420a5f0e72de8e1
Laurent Jakubina <laurentjakubina@gmail.com>
2020-03-17 23:59:28 -0400

    improved bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,21 @@
 # Author: ljak
 # Creation Date: 2020_03_16
 
-# Description: Script that I'm using to bootstrap any new Debian-based Linux system. Its applies last updates, sets keyboard layouts to BÉPO (dvorak similar layout optimized for French language) and US, install git and chezmoi. Finaly, chezmoi will take care to execute the next scripts to ensure the bootstrapping procedure.
+# Description: Script that I'm using to bootstrap any new Debian-based Linux system. Its applies last updates, sets keyboard layouts to BÉPO (dvorak similar layout optimized for French language) and US, install git and chezmoi. Next, chezmoi take care of the bootstrapping procedure to the end.
 
 # Run: Execute command `./bootstrap.sh`
+
+set -o errexit  # make script exits when a command fails
+set -o pipefail # exit status of the last command that threw a non-zero exit code is returned
+# set -o xtrace  # for debugging
+
+source set_kb_layouts.sh
 
 sudo apt update -y
 sudo apt upgrade -y
 
-source set_kb_layouts.sh
-
 sudo apt install -y git
-
 sudo snap install chezmoi --classic
+
+chezmoi init https://github.com/ljak/dotfiles.git
+# chezmoi apply
