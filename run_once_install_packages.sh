f493b7a67ebd342aa5800ca19034e96171efc56d
Laurent Jakubina <laurentjakubina@gmail.com>
2020-03-21 00:27:29 -0400

    wrote run_once_install_packages.sh

---
+++ run_once_install_packages.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# Author: ljak
+# Creation Date: 2020_03_13
+
+# Description: 
+
+# Run: 
+
+set -o errexit  # make script exits when a command fails
+set -o pipefail # exit status of the last command that threw a non-zero exit code is returned
+# set -o xtrace  # for debugging
+
+
+# Set up git profil
+# ssh keys, github, gitlab, ...
+# 
+
+
+# Install packages that does not require additional commands
+packages_wo_addcomms="
+curl
+htop
+wget
+micro
+xclip
+firefox
+ca-certificates
+apt-transport-https
+software-properties-common
+"
+sudo apt install -y ${packages_wo_addcomms}
+
+
+# Install packages that require additional commands
+
+# zsh
+sudo apt install -y zsh
+chsh -s /bin/zsh  # set default shell
+
+# tilix
+sudo apt install -y tilix dconf
+# set up personnal settings with dconf
+
+# pet
+# install ?
+# gist ?
+
+# Sublime Text and Sublime Merge
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update -y
+sudo apt install -y sublime-text sublime-merge
+ # Next commands are necessary to set up my personnal settings
+subl  # start sublime to create .config/ inside home
+sudo wget -P "/home/ljak/.config/sublime-text-3/Installed Packages" https://packagecontrol.io/Package%20Control.sublime-package
+cd ~/.config/sublime-text-3/Packages/User/
+git init .
+git remote add origin https://github.com/ljak/STBackup
+git pull origin master
+cd
+ # Make sublime default editor in terminal and as app (visual)
+EDITOR=subl; export EDITOR
+VISUAL="$EDITOR"; export VISUAL
+
+
+# Python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install -y python3 # bpython ? python3-pip ?
+
+# Docker
+sudo apt install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
+# Install packages that does not use apt
+sudo snap install pycharm-community --classic
+# personnal settings from repo with gist
