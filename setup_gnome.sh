fec83fa1683a76769ffa2c922ee343ff3aecaa13
Laurent Jakubina <laurentjakubina@gmail.com>
2020-03-26 00:37:07 -0400

    improved setup_gnome.sh

--- setup_gnome.sh
+++ setup_gnome.sh
@@ -10,22 +10,18 @@
 # GNOME Shell Extensions
 sudo add-apt-repository universe
 sudo apt update -y
-sudo apt install -y gnome-tweak-tool gnome-shell-extensions chrome-gnome-shell
+sudo apt install -y gnome-tweak-tool chrome-gnome-shell
+curl https://raw.githubusercontent.com/oae/gnome-shell-extensions-sync/master/installer.sh | bash  # Sync extension auto-install
+# See Gist ID and Gist Token to auto sync
 xdg-open https://addons.mozilla.org/en-US/firefox/addon/gnome-shell-integration/  # open link in default browser
+
+# if needed
 # gnome-shell --version
-xdg-open https://extensions.gnome.org/extension/1486/extensions-sync/  # setup with GIST ?
+# sudo rm -r /usr/share/gnome-shell/extensions/ubuntu-* / pomodoro
 # More http://www.bernaerts-nicolas.fr/linux/76-gnome/345-gnome-shell-install-remove-extension-command-line-script
 
-# + dconf ? copy .local/share/gnome-shell/extensions ?
 
-# https://extensions.gnome.org/extension/1160/dash-to-panel/
-# https://extensions.gnome.org/extension/517/caffeine/
-# https://extensions.gnome.org/extension/53/pomodoro/
-# https://extensions.gnome.org/extension/750/openweather/
-# https://extensions.gnome.org/extension/19/user-themes/
-# https://extensions.gnome.org/extension/906/sound-output-device-chooser/
-# Configure Dash to Panel as follows: ... These settings can also be imported from the file configs/dash-to-panel
-
+# TODO get the settings for the extensions ?
 
 # ### GNOME Theme
 # GTK Theme
@@ -37,11 +33,9 @@ sudo apt install -y materia-gtk-theme
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt update -y
 sudo apt install -y papirus-icon-theme
-echo "Need to install the hardcode-fixer: https://github.com/Foggalong/hardcode-fixer"
-	# TODO call the hardcoded_icon_fixer.sh
-	# chmod +x hardcoded_icon_fixer.sh
-	# sudo ./hardcoded_icon_fixer.sh 
-	# TODO try the hardcoded TRAY fix ?
+# fix application icons with https://github.com/Foggalong/hardcode-fixer
+sudo ./hardcoded_icon_fixer.sh
+# TODO try the hardcoded TRAY fix ?
 
 
 # GNOME/Ubuntu Pomodoro
@@ -54,6 +48,7 @@ sudo apt install -y gnome-shell-pomodoro
 
 
 # Add to Favourites
+dconf write /org/gnome/shell/favorite-apps "['']"  # Clear favorites
 add_to_favourites () {
 	application="'${1}.desktop'"
 	favourites="/org/gnome/shell/favorite-apps"
@@ -63,14 +58,9 @@ add_to_favourites () {
       | sed "s/${application}//g" \
       | sed -e "s/]$/, ${application}]/")"
 }
-add_to_favourites tilix
-# System Monitor
-# Files Manager
-# Terminal
-# Firefox
-# Sublime Text
-# Sublime Merge
-# Code (Visual Studio)
-# Spotify
-# Gnome Screenshot
-# Gnome Clocks
+add_to_favourites com.gexperts.Tilix  # Terminal
+add_to_favourites org.gnome.Nautilus  # Files Manager
+add_to_favourites firefox
+add_to_favourites sublime_text
+add_to_favourites sublime_merge
+add_to_favourites org.gnome.Screenshot
