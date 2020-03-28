eed0797072ee52446c7f46ef5f2137fd41d463da
Laurent Jakubina <laurentjakubina@gmail.com>
2020-03-28 15:28:14 -0400

    moved bootstrap scripts to a specific folder

---
+++ bootstrap_scripts/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Author: ljak
+# Creation Date: 2020_03_16
+
+# Description: Script that I'm using to bootstrap any new Debian-based Linux system. Its sets keyboard layouts to BÉPO (dvorak similar layout optimized for French language) and US, applies last updates and install git, chezmoi and bitwarden. After what, chezmoi takes care of the rest of the bootstrapping procedure.
+
+# Run: Execute command `./bootstrap.sh`
+
+
+source set_kb_layouts.sh
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install -y git snapd
+sudo snap install chezmoi --classic
+sudo snap install bw  # bitwarden
+
+bw login
+
+chezmoi init https://github.com/ljak/dotfiles.git
+# chezmoi apply
