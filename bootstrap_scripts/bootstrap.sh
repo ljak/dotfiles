b83c40c241d1b1a8134859d0ac454d7ce366100f
Laurent Jakubina <laurentjakubina@gmail.com>
2020-04-04 19:41:00 -0400

    merged set_kb_layouts.sh into bootstrap.sh

--- bootstrap_scripts/bootstrap.sh
+++ bootstrap_scripts/bootstrap.sh
@@ -1,13 +1,52 @@
 #!/usr/bin/env bash
 # Author: ljak
-# Creation Date: 2020_03_16
+# Creation Date: 2019_11_26
 
 # Description: Script that I'm using to bootstrap any new Debian-based Linux system. Its sets keyboard layouts to BÉPO (dvorak similar layout optimized for French language) and US, applies last updates and install git, chezmoi and bitwarden. After what, chezmoi takes care of the rest of the bootstrapping procedure.
 
 # Run: Execute command `./bootstrap.sh`
 
 
-source set_kb_layouts.sh
+set -o pipefail  # script failed when one command of a pipe failed
+
+
+get_current_desktop_env () {
+	local desktop=${XDG_CURRENT_DESKTOP}
+	if [ "${desktop}" = "" ]; then
+		desktop=$(echo ${XDG_DATA_DIRS} | sed -rn 's/.*(xfce|kde|gnome).*/\1/')
+	fi
+	echo ${desktop,,}  #  convert to lowercase
+}
+
+# Hint: There is a terminal configuration tool if needed: `sudo dpkg-reconfigure keyboard-configuration`
+set_keyboard_layouts () {
+	local layouts='fr,us'
+	local variant='bepo,'
+	local options='grp:rctrl_toggle,terminate:ctrl_alt_bksp,grp_led:scroll'
+
+	# set xkb config file
+	local debian_kb_settings_filepath=/etc/default/keyboard
+	if [ ! -f "${debian_kb_settings_filepath}" ]; then
+		echo 'ERROR from set_kb_layouts.sh: file '${debian_kb_settings_filepath}' does not exist'
+		exit 1
+	fi
+	sudo sed -i 's/XKBLAYOUT=.*$/XKBLAYOUT="'${layouts}'"/g' ${debian_kb_settings_filepath}
+	sudo sed -i 's/XKBVARIANT=.*$/XKBVARIANT="'${variant}'"/g' ${debian_kb_settings_filepath}
+	sudo sed -i 's/XKBOPTIONS=.*$/XKBOPTIONS="'${options}'"/g' ${debian_kb_settings_filepath}
+	echo 'XKB config file '${debian_kb_settings_filepath}' SETTED.'
+
+	# set GNOME GSetting variables if needed
+	local current_desktop_env=$(get_current_desktop_env)
+	if [[ ${current_desktop_env} = *"gnome"* ]]; then
+		sudo gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'fr+bepo'), ('xkb', 'us')]"
+		sudo gsettings set org.gnome.desktop.input-sources xkb-options "['${options}']"
+		echo 'GNOME Gsetting org.gnome.desktop.input-sources SETTED.'
+	fi
+}
+
+
+set_keyboard_layouts
+
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -18,7 +57,7 @@ sudo snap install bw  # bitwarden
 
 bw login
 
-chezmoi init https://github.com/ljak/dotfiles.git
-# chezmoi apply
-
+chezmoi init git@github.com:ljak/dotfiles.git
+# chezmoi init https://github.com/ljak/dotfiles.git
 # git remote set-url origin git@github.com:ljak/dotfiles.git could be needed later
+chezmoi apply
