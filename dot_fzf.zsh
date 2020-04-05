0814eee120fe0272a9375631aed3c9609ba6c6f9
Laurent Jakubina <laurentjakubina@gmail.com>
2020-04-05 15:58:19 -0400

    fixed double colons in file paths from .fzf.zsh

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -6,8 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "~/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source ~/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh 2> /dev/null
 
 # Key bindings
 # ------------
-source "~/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh"
+source ~/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh
