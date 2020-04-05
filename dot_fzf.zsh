5929a62774f3960e6af4018bdd5e84ab7880fdef
Laurent Jakubina <laurentjakubina@gmail.com>
2020-04-05 14:12:37 -0400

    fixed .fzf.zsh to make it usefull into any user home folder, aka /home/user to ~

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/ljak/.oh-my-zsh/custom/plugins/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/ljak/.oh-my-zsh/custom/plugins/fzf/bin"
+if [[ ! "$PATH" == *~/.oh-my-zsh/custom/plugins/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}~/.oh-my-zsh/custom/plugins/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/ljak/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/ljak/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh"
+source "~/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh"
